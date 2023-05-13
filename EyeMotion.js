7e17ef917592a32957b928ef2621822200708f76
Yash Tonger <112475138+yashTonger@users.noreply.github.com>
2023-05-13 18:57:39 +0530

    Update EyeMotion.js
    Added Mosquito bite, eye closing transition and and improved grid loop

--- EyeMotion.js
+++ EyeMotion.js
@@ -2,21 +2,52 @@ var Totalrows= 30;
 var Totalcolumn= 30;
 var grid = document.getElementById("grid");
 
-for(let r = 1;r<=Totalrows;r++) {
-    for(let c = 1;c<=Totalcolumn;c++) {
-     
-        grid.innerHTML +='<div id="'+r+c+'" class="cell">'+r+c+'</div>'; 
+
+//adding grid
+    for(let r = 1;r<=Totalrows;r++) {
+        for(let c = 1;c<=Totalcolumn;c++) {
+        
+            grid.innerHTML +='<div id="id'+r+c+'" class="cell"></div>'; 
+        }
     }
-}
 
-        const allCells=document.getElementsByClassName("cell");
-        console.log(allCells);
-        const allCellsArray = Array.from(allCells)
+// mosquto biting
+    const allCells=document.getElementsByClassName("cell");
+    const allCellsArray = Array.from(allCells)
 
-        allCellsArray.forEach( function(currentCell) {
-            
-            currentCell.addEventListener('click',() =>{
-                currentCell.style.background='red';
-            });
+    allCellsArray.forEach( function(currentCell, index ) {
+        
+        currentCell.addEventListener('click',() =>{
+            currentCell.classList.add('marked') ;
         });
+    });
 
+
+ //eyes closing
+document.getElementById("leftEye").addEventListener('mouseover',() => {
+    document.getElementById("leftEyeLid").classList.add('closed');
+    document.getElementById("rightEyeLid").classList.add('closed');
+}
+);
+document.getElementById("leftEye").addEventListener('mouseout',() => {
+    setTimeout(() =>{
+        document.getElementById("leftEyeLid").classList.remove('closed');
+        document.getElementById("rightEyeLid").classList.remove('closed');
+    }
+    ,1000);
+}
+);
+document.getElementById("rightEye").addEventListener('mouseover',() => {
+    document.getElementById("leftEyeLid").classList.add('closed');
+    document.getElementById("rightEyeLid").classList.add('closed');
+}
+);
+document.getElementById("rightEye").addEventListener('mouseout',() => {
+    setTimeout(()=>{
+        document.getElementById("leftEyeLid").classList.remove('closed');
+        document.getElementById("rightEyeLid").classList.remove('closed');
+    },1000);
+}
+);
+
+// eye following target
