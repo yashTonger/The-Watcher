379345f2a52617a214d758007015e1ddf008d7ac
Yash Tonger <112475138+yashTonger@users.noreply.github.com>
2023-06-19 22:25:10 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,8 @@
     var isPause= false;
     var isLight = false;
     var isEnd = false;
-    var isSpacePressed= false;
+    var isMotionStoped= false;
+    var isFlownAway=false;
     var isEyeClosed =false;
     var ismute= false;
     var isBiting= -1;
@@ -13,6 +14,15 @@
     var grid = document.getElementById("grid");
     var blood = 0;  
 
+    var mosquito =  document.getElementById("mosquito1");
+    var Rules = " <h1>Welcome to the Game!</h1>"+
+    "<p>Follow the instructions below to play:</p>"+
+    "<ol>"+
+      "<li>Step 1: Choose a difficulty level</li>"+
+      "<li>Step 2: Click the 'Start' button to begin</li>"+
+      "<li>Step 3: Play the game using the arrow keys</li>"+
+      "<li>Step 4: Have fun and aim for the high score!</li> "+
+    "</ol>";
 
 //adding grid
     for(let r = 1;r<=Totalrows;r++) {
@@ -21,25 +31,6 @@
             grid.innerHTML +='<div id="id'+r+c+'" class="cell"></div>'; 
         }
     }
-
-
-
-// audio
-// var isPlaying = false;
-// document.getElementById("test").addEventListener("click",() => 
-//     {
-//         if(!isPlaying){
-//             document.getElementById("back-audio").play();
-//             document.getElementById("outer-ring").classList.add("unmuted");
-//             isPlaying= true;
-//         }
-//         else{
-//             document.getElementById("back-audio").pause();
-//             document.getElementById("outer-ring").classList.remove("unmuted");
-//             isPlaying=false;
-//         }
-//     }
-// );
 
 
 
@@ -54,7 +45,7 @@
     //bitting
     function bit(currentCell,index) {
         console.log("bit");
-        if(!isSpacePressed && !isPause && isStarted && !isEnd)
+        if(!isMotionStoped && !isPause && isStarted && !isEnd && !isFlownAway)
         {
             stopMonment();
             
@@ -78,7 +69,7 @@
                             }
                         }
                     }
-                },4000);
+                },2000);
                 isBiting=index;
             }
         }
@@ -101,13 +92,13 @@
             document.getElementById("light-filter").style.background = "rgba(0,0,0,0)";
 
             setTimeout(() =>{
-                if(isSpacePressed){
+                if(isFlownAway){
                     lightOff();
                 }
                 else{
                     kill();
                 }
-            },5000);
+            },4000);
         }
 
     }
@@ -122,31 +113,63 @@
 
     }
     
+
     // stop mosqito animation
     function stopMonment() {
-        if(isStarted && !isPause && !isSpacePressed){
-            isSpacePressed=true;
-            document.getElementById("mosquito1").classList.remove("fly");
+        if(isStarted && !isPause && !isMotionStoped){
+            isMotionStoped=true;
+            mosquito.classList.remove("fly");
             if(!ismute){
                 //pasue mosquito sound
             }
         }
 
     }
+
+
+
      // start mosqito animation
      function startMonment() {
-        if(isStarted && !isPause && isSpacePressed){
-            isSpacePressed=false;
-            document.getElementById("mosquito1").classList.add("fly");
+        if(isStarted && !isPause && isMotionStoped){
+            isMotionStoped=false;
+            mosquito.classList.add("fly");
             if(!ismute){
                 //play mosquito sound
             }
         }
     }
 
+    //fly away 
+    function flyAway() {
+        if(isStarted && !isPause && !isFlownAway){
+            isFlownAway=true;
+            mosquito.classList.remove("flyBack");
+            mosquito.classList.add("flyAway");
+            if(!ismute){
+                //pasue mosquito sound
+            }
+        }
+
+    }
+
+
+    //fly back
+    function flyBack() {
+        if(isStarted && !isPause && isFlownAway){
+            isFlownAway=false;
+            mosquito.classList.remove("flyAway");
+            mosquito.classList.add("flyBack");
+            if(!ismute){
+                //play mosquito sound
+            }
+        }
+    }
+
+
+
      // mosqito animation
      function mosquitoMotion(e) {
-        if(!isEnd && !isSpacePressed){
+        if(!isEnd && !isMotionStoped){
             var doc = document.documentElement;
             var eyeX = (((doc.clientWidth-e.clientX)/doc.clientWidth)*100)/(5*1.1414);
             var eyeY = ((((doc.clientHeight/2) - e.clientY)/doc.clientWidth)*100)/(5*1.1414);
@@ -156,8 +179,8 @@
             var rightEyeBallLeft = (rightEyeX - eyeY)/1.414;
             var rightEyeBallTop = (rightEyeX + eyeY)/1.414;
 
-            document.getElementById("mosquito1").style.left= e.clientX   + "px";
-            document.getElementById("mosquito1").style.top= e.clientY +1 + "px";
+            mosquito.style.left= e.clientX   + "px";
+            mosquito.style.top= e.clientY +1 + "px";
             document.getElementById("leftEyeBall").style.left= eyeBallLeft + 2 + "vw";
             document.getElementById("leftEyeBall").style.top= eyeBalltop + 2 + "vw";
             document.getElementById("rightEyeBall").style.left= rightEyeBallLeft  + 2  + "vw";
@@ -192,19 +215,16 @@
         if(isStarted && !isPause && !isEnd ){
 
             isPause=true;
-            document.getElementById('dashboard').style.visibility="visible";
-            document.getElementById('replay').style.visibility="visible";
-            document.getElementById('resume').style.visibility="visible";
-            document.getElementById('mute').style.visibility="visible";
+        
+            show('dashboard');
+            show('replay');
+            show('mute');
+            show('resume');
             
-            document.getElementById('replay').classList.remove('disable');
-            document.getElementById('resume').classList.remove('disable');
-            document.getElementById('mute').classList.remove('disable');
-            
-            document.getElementById('pause').classList.add('disable');
-            document.getElementById('pause').style.visibility="hidden";
+            hide('score-bar');
+            hide('pause');
 
-        }
+        }   
         
     }
 
@@ -213,18 +233,13 @@
         if(isStarted && isPause && !isEnd ){
             
             isPause=false;
-            document.getElementById('dashboard').style.visibility="hidden";
-            document.getElementById('replay').style.visibility="hidden";
-            document.getElementById('resume').style.visibility="hidden";
-            document.getElementById('mute').style.visibility="hidden";
-            
-            document.getElementById('replay').classList.add('disable');
-            document.getElementById('resume').classList.add('disable');
-            document.getElementById('mute').classList.add('disable');
+            hide('dashboard');
+            hide('replay');
+            hide('mute');
+            hide('resume');
 
-
-            document.getElementById('pause').classList.remove('disable');
-            document.getElementById('pause').style.visibility="visible";
+            show('score-bar');
+            show('pause');
         }
     }
 
@@ -246,7 +261,7 @@
         isPause=false;
         ismute=false;
         isEnd=false;
-        isSpacePressed=false;
+        isMotionStoped=false;
         blood=0;
 
 
@@ -256,31 +271,24 @@
         lightOff();
 
         //buttons config
-            document.getElementById('replay').style.visibility="hidden";
-            document.getElementById('resume').style.visibility="hidden";
-            document.getElementById('mute').style.visibility="hidden";
-            document.getElementById('go').style.visibility="hidden";
-            document.getElementById('pause').style.visibility="visible";
-            
-            if(!document.getElementById('replay').classList.contains('disable')){
-                document.getElementById('replay').classList.add('disable');
-            }
-            if(!document.getElementById('resume').classList.contains('disable')){
-                document.getElementById('resume').classList.add('disable');
-            }
-            if(!document.getElementById('mute').classList.contains('disable')){
-                document.getElementById('mute').classList.add('disable');
-            }
-            if(!document.getElementById('go').classList.contains('disable')){
-                document.getElementById('go').classList.add('disable');
-            }
-            if(document.getElementById('pause').classList.contains('disable')){
-                document.getElementById('pause').classList.remove('disable');
-            }
-            
+        hide('dashboard');
+        hide('replay');
+        hide('mute');
+        hide('resume');
+        hide('go');
+        hide('failPic');
+        hide('winPic');
+
+        show('pause');
+        show("mosquito1");
+        show("score-bar");
 
 
-        
+        document.getElementById('content').innerHTML= Rules;
+    
+
+
+
         //remove old marks
         for(let r = 1;r<=Totalrows;r++) {
             for(let c = 1;c<=Totalcolumn;c++) {
@@ -300,6 +308,8 @@
     function end(result) {
         //is satrted - false 
         console.log(result);
+        document.getElementById('content').innerHTML= result;
+        show('go');
     }
     
     // win
@@ -308,8 +318,17 @@
 
             stopMonment();
             isEnd=true;
-            console.log("win-animation");
-            end("win");
+
+
+            hide('score-bar');
+            hide('pause');
+            hide("mosquito1");
+            document.getElementById('content').innerHTML= " ";
+            show('dashboard');
+            show('winPic');
+            setTimeout(()=>{
+                end("MISSION PASSED");
+            },2000);
         }
     }
 
@@ -319,8 +338,16 @@
 
             stopMonment();
             isEnd=true;
-            console.log("kill-animation");
-            end("kill");
+            
+            hide('score-bar');
+            hide('pause');
+            hide("mosquito1");
+            document.getElementById('content').innerHTML= " ";
+            show('dashboard');
+            show('failPic');
+            setTimeout(()=>{
+                end("MISSION FAILED");
+            },2000);
         }
     }
 
@@ -329,14 +356,28 @@
         if(ismute){
             console.log("un-mute");
             ismute=false;
-            document.getElementById("outer-ring").classList.add("unmuted");
+            
         }
         else {
             console.log("mute");
             ismute=true;
-            document.getElementById("outer-ring").classList.remove("unmuted");
+           
         }
 
+    }
+
+    //show element
+    function show(element) {
+        document.getElementById(element).style.visibility="visible";
+        document.getElementById(element).classList.remove('disable')
+    }
+
+    //hide and disable the element
+    function hide(element) {
+        if(!document.getElementById(element).classList.contains('disable')){
+            document.getElementById(element).classList.add('disable');
+        }
+        document.getElementById(element).style.visibility="hidden";
     }
 
 
@@ -383,11 +424,30 @@
     //mute bottons
 
     document.getElementById("mute").addEventListener('click',mute);
-
     //space button
 
-    document.addEventListener('keydown',stopMonment);
-    document.addEventListener('keyup', startMonment);
+    document.addEventListener('keydown', function(event) {
+        if (event.code === 'Space') {
+          flyAway();
+        }
+      });
+      
+      document.addEventListener('keyup', function(event) {
+        if (event.code === 'Space') {
+          flyBack();
+        }
+      });
+
     //mouse position
 
     document.documentElement.addEventListener('mousemove', mosquitoMotion);
+
+
+
+    //removing animation
+
+        mosquito.addEventListener('animationend', function(event) {
+        if (event.animationName === 'flyBack') {
+            mosquito.classList.remove("flyBack");
+        }
+        });
